bf04ebd1fc4207f7d9821a988f5d3dbf3af93354
benjaminisa <bennyearts@gmail.com>
2024-07-03 22:23:59 +0100

    Initial Commit

---
+++ LibraryManagementSystem/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            User user = new User("Benjamin");
+
+            while (true)
+            {
+                Console.WriteLine("Library Management System"); // A welcome menu-interface
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Borrow Book");
+                Console.WriteLine("3. Return Book");
+                Console.WriteLine("4. View All Books");
+                Console.WriteLine("5. Exit");
+                Console.Write("Select an option: ");
+                var choice = Console.ReadLine();
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case "1":
+                            AddBook(library);
+                            break;
+                        case "2":
+                            BorrowBook(library, user);
+                            break;
+                        case "3":
+                            ReturnBook(library, user);
+                            break;
+                        case "4":
+                            library.ViewAllBooks();
+                            break;
+                        case "5":
+                            return;
+                        default:
+                            Console.WriteLine("\nInvalid choice. Please select again.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nAn error occurred: {ex.Message}");
+                }
+            }
+        }
+
+        static void AddBook(Library library)
+        {
+            Console.Write("\nEnter Title: ");
+            string title = Console.ReadLine();
+            Console.Write("\nEnter Author: ");
+            string author = Console.ReadLine();
+            Console.Write("\nEnter ISBN: ");
+            string isbn = Console.ReadLine();
+
+            Book book = new Book(title, author, isbn, 0);
+            library.AddBook(book);
+        }
+
+        static void BorrowBook(Library library, User user)
+        {
+            Console.Write("\nEnter book ID: ");
+            if (int.TryParse(Console.ReadLine(), out int bookId))
+            {
+                user.BorrowBook(library, bookId);
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid book ID.");
+            }
+        }
+
+        static void ReturnBook(Library library, User user)
+        {
+            Console.Write("\nEnter book ID: ");
+            if (int.TryParse(Console.ReadLine(), out int bookId))
+            {
+                user.ReturnBook(library, bookId);
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid book ID.");
+            }
+        }
+    }
+}
+
---
+++ LibraryManagementSystem/User.cs
@@ -0,0 +1,31 @@
+﻿using LibraryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LibraryManagementSystem
+{
+    public class User
+    {
+        public string Name { get; private set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void BorrowBook(Library library, int bookId)
+        {
+            library.BorrowBook(bookId, this);
+        }
+
+        public void ReturnBook(Library library, int bookId)
+        {
+            library.ReturnBook(bookId, this);
+        }
+    }
+}
+
